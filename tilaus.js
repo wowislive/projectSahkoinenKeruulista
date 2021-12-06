7008a39cd7a8a3afadad0ec87eb64b59f0c8a63b
Dmitrii Pavlov <wowislive@gmail.com>
2021-12-06 19:16:55 +0200

    Update tilaus.js and tilaus.html

--- tilaus.js
+++ tilaus.js
@@ -1,6 +1,9 @@
 let orderData = JSON.parse(localStorage.order);
-let orderID = "comment" + orderData.orderid;
+let commentId = "comment" + orderData.orderid;
+let orderId = "order" + orderData.orderid;
 let text = "";
+let productBtnDone = "";
+let localProductCheck = [];
 
 //function to build the order table
 function tableConstruct() {
@@ -36,13 +39,23 @@ function tableConstruct() {
   document.getElementById("orderTable").innerHTML = tableContent;
 }
 
-//function for building cards with ordered products
+//function for building cards with ordered products. storing the product key in the localstorage
 function constructProductsList() {
   let productsList = "";
-  let productInteract = "";
   orderData.products.forEach(function (key) {
+    let x = key.code;
+    let replaced = x.replaceAll(" ", "");
+    localProductCheck.push(replaced);
+    productBtnDone =
+      "<div><input type='button' value='Mark us done' onclick=productDone(\"" +
+      replaced +
+      "\") /><input type='button' value='Product not ready' onclick=productNotReady(\"" +
+      replaced +
+      '") /></div>';
     productsList +=
-      "<div><table class = 'products'><tr><th>Product</th><td>" +
+      "<div><table class = 'products' id =\"" +
+      replaced +
+      '"><tr><th>Product</th><td>' +
       key.product +
       "</td></tr><tr><th>Description</th><td>" +
       key.description +
@@ -56,22 +69,49 @@ function constructProductsList() {
       key.suppliercode +
       "</td></tr><tr><th>Price per unit</th><td>" +
       key.unit_price +
-      "</td></tr></table></div><br />";
+      "</td></tr></table></div>" +
+      productBtnDone;
   });
   document.getElementById("products").innerHTML = productsList;
+}
+
+//marks the table in green if the order is complete
+function orderDone() {
+  localStorage.setItem(orderId, "done");
+  document.getElementById("orderTable").style.backgroundColor = "#ccffdc";
+}
+
+//marks the table in red if the order is not completed
+function orderNotReady() {
+  localStorage.setItem(orderId, "notReady");
+  document.getElementById("orderTable").style.backgroundColor = "#ffcccc";
+}
+
+//marks the product card in green if the product is completed
+function productDone(x) {
+  let productCode = "product" + x;
+  localStorage.setItem(productCode, "done");
+  document.getElementById(x).style.backgroundColor = "#ccffdc";
+}
+
+//marks the product card in red if the order is not completed
+function productNotReady(x) {
+  let productCode = "product" + x;
+  localStorage.setItem(productCode, "notReady");
+  document.getElementById(x).style.backgroundColor = "#ffcccc";
 }
 
 //function to post a comment. Store this comment in local storage
 function sendComment() {
   let comment = document.getElementById("commentField").value;
   if (comment != "") {
-    text = localStorage.getItem(orderID);
+    text = localStorage.getItem(commentId);
     if (text != null) {
       text += comment + "<br />";
     } else {
       text = comment + "<br />";
     }
-    localStorage.setItem(orderID, text);
+    localStorage.setItem(commentId, text);
     document.getElementById("commentHere").innerHTML = text;
     document.getElementById("commentField").value = "";
   }
@@ -79,14 +119,34 @@ function sendComment() {
 
 //function to delete comments
 function clearComments() {
-  localStorage.setItem(orderID, "");
+  localStorage.setItem(commentId, "");
   document.getElementById("commentHere").innerHTML = "";
+}
+
+//checks local storage and marks tables in color. also puts comments on the page
+function checkLocal() {
+  document.getElementById("commentHere").innerHTML =
+    localStorage.getItem(commentId);
+  if (localStorage.getItem(orderId) == "done") {
+    document.getElementById("orderTable").style.backgroundColor = "#ccffdc";
+  } else if (localStorage.getItem(orderId) == "notReady") {
+    document.getElementById("orderTable").style.backgroundColor = "#ffcccc";
+  }
+  for (i = 0; i < localProductCheck.length; i++) {
+    let productCode = "product" + localProductCheck[i];
+    if (localStorage.getItem(productCode) == "done") {
+      document.getElementById(localProductCheck[i]).style.backgroundColor =
+        "#ccffdc";
+    } else if (localStorage.getItem(productCode) == "notReady") {
+      document.getElementById(localProductCheck[i]).style.backgroundColor =
+        "#ffcccc";
+    }
+  }
 }
 
 //a function that runs when the page is fully loaded
 function init() {
-  document.getElementById("commentHere").innerHTML =
-    localStorage.getItem(orderID);
   tableConstruct();
   constructProductsList();
+  checkLocal();
 }
